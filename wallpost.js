db8ccb83d307bf22647701ad9aa6d3123711c36c
Rafi Jacoby <rjacoby@astrokid.com>
2013-04-04 15:19:30 -0500

    fixed the response status checking, added an img

--- wallpost.js
+++ wallpost.js
@@ -1,7 +1,7 @@
 function toggleContent() {
   FB.getLoginStatus(function(response) {
     pickerDiv = $('#picker');
-    if (response.session) {
+    if (response.status == 'connected') {
       pickerDiv.html("<div class='prompt'>Type a friend\'s name to select them for wall post.</div><div id='selector'><div id='selectorInput' style='display: inline-block; height:48px;'><input id='fbAutocomplete' size=50 style='font-size: 18px;' /></div><div id='postButton' display='none' style='display: none;'><a id='postButtonLink' href='#'>Post to Wall</a></div></div>");
       setupFriendCompleter("input#fbAutocomplete");
       
@@ -29,6 +29,7 @@ function doWallPost() {
      name: 'Posting to a friend\'s wall using the pure Javascript friend picker',
      link: 'http://rjacoby.github.com/friendcompleter/',
      caption: 'The joy of using a pure JS friend picker.',
+     picture: 'http://2.bp.blogspot.com/-iarq5sjWDWc/TWRxt8nPegI/AAAAAAAAA_0/ABabl2TlOO0/s1600/emu.jpg',
      description: 'This post is coming from a test app using a pure Javascript friend picker. The picker provides typeahead lookup in the browser with no server-side component.',
      to: $('input#fbFriendId').val()
     }
